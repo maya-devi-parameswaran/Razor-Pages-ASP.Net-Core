1801a5b5b183a3a1a9ad9d310fadbbc38c79aa46
maya-devi-parameswaran <60831976+maya-devi-parameswaran@users.noreply.github.com>
2022-06-08 11:13:00 +1000

    changes

---
+++ PopularBrews.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Razor_Pages_ASP.Net_Core.Services;
+
+namespace Razor_Pages_ASP.Net_Core
+{
+    public class PopularBrews : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(int count)
+        {
+            var menu = new MenuService();
+            return View( menu.GetMenuItems().Take(count));
+        }
+    }
+}
